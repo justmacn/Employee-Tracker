52c2411d030ce05443a45aab52771bd2b488b273
justmacn <172618812+justmacn@users.noreply.github.com>
2024-08-04 15:34:04 -0400

    added all queries to db/index.js

--- db/index.js
+++ db/index.js
@@ -16,21 +16,74 @@ class DB {
   }
 
   // selects all departments 
-  viewAllDepartments() {
-    return this.query(
-      'SELECT department.id, department.name FROM department;')
-  }
+  selectAllDepartments() {
+    return this.query(`
+  SELECT
+    department.id, department.name
+  FROM department;
+  `)}
 
   // selects all roles w/ department
-  viewAllRoles() {
-    return this.query(
-      'SELECT role.id, role.title AS job_title, department.name AS department, role.salary FROM role JOIN department ON role.department_id = department.id;')
-  }
+  selectAllRoles() {
+    return this.query(`
+  SELECT 
+    role.id, 
+    role.title AS job_title, 
+    department.name AS department, 
+    role.salary 
+  FROM role 
+  JOIN department ON role.department_id = department.id;
+  `)}
 
   // selects all employees w/ role + department
-  viewAllEmployees() {
-    return this.query(
-      'SELECT employee.id, CONCAT(employee.rank, " ", employee.first_name, " ", employee.last_name) AS name, role.title AS job_title, department.name AS department, role.salary, employee.manager_id AS manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id;')
-  }
+  selectAllEmployees() {
+    return this.query(`
+  SELECT 
+    employee.id, 
+    CONCAT(employee.rank, ' ', employee.first_name, ' ', employee.last_name) AS name, 
+    role.title AS job_title, 
+    department.name AS department, 
+    role.salary, 
+    employee.manager_id AS manager 
+  FROM employee 
+  JOIN role ON employee.role_id = role.id 
+  JOIN department ON role.department_id = department.id;
+  `)}
 
+  // inserts a new department into table
+  insertDepartment(departmentName) {
+    return this.query(`
+  INSERT INTO 
+    department(name) 
+  VALUES ($1);`, [departmentName]
+  )}
+
+  //inserts a new role into table
+  insertRole(role) {
+    const { title, salary, department_id} = role
+    return this.query(`
+  INSERT INTO 
+    role(title, salary, department_id) 
+  VALUES ($1, $2, $3);`, [title, salary, department_id]
+  )}
+
+  //inserts a new employee into table
+  insertEmployee(employee) {
+    const { rank, first_name, last_name, role_id, manager_id} = employee
+    return this.query(`
+  INSERT INTO 
+    employee(rank, first_name, last_name, role_id, manager_id) 
+  VALUES ($1, $2, $3, $4, $5);`, [rank, first_name, last_name, role_id, manager_id]
+  )}
+
+  //updates an employee role from the table
+  updateEmployee(employeeID, roleID) {
+    return this.query(`
+  UPDATE 
+    employee 
+  SET role_id = $1 
+  WHERE id = $2;`, [employeeID, roleID]
+  )}
 }
+
+module.exports = DB()
